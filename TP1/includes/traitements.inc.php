09f832ebf4be030c7e727449afbed90ce41e577f
David MOLINARI <davvmoli@gmail.com>
2016-11-17 11:34:48 +0100

    add symfony debug+dump + autoloader

--- TP1/includes/traitements.inc.php
+++ TP1/includes/traitements.inc.php
@@ -5,7 +5,7 @@ function recuperationUnClient($unObjetPdo, $id){
   $ligne->bindValue('pnocli',$id,PDO::PARAM_INT);
   $ligne->execute();
   $unClient = $ligne->fetch(PDO::FETCH_OBJ);
-  var_dump($unClient);
+  dump($unClient);
 }
 
 function afficheTousClients($unObjetPdo)
@@ -14,19 +14,36 @@ function afficheTousClients($unObjetPdo)
   $lignes = $unObjetPdo->query($sql);
   $lignes->setFetchMode(PDO::FETCH_OBJ);
   while ($unClient = $lignes->fetch()) {
-    echo "Numéro du client : " . '<strong>' . $unClient->NOCLI . '</strong>' . " Nom du client : " .  '<strong>' .$unClient->NOMCLI . '</strong>'
+    echo 'Numéro du client : ' . '<strong>' . $unClient->NOCLI . '</strong>' . " Nom du client : " .  '<strong>' .$unClient->NOMCLI . '</strong>'
     . " Prénom du client " . '<strong>' .$unClient->PRENOMCLI . '</strong>'. "<br>";
   }
 }
 
-function  recupUnObjetClient($unObjetPdo, $id){
+function recupUnObjetClient($unObjetPdo,$id){
   $sql = "select * from CLIENT where NOCLI=:pnocli";
-  $lignes = $unObjetPdo->prepare($sql);
-  $lignes->bindValue(':pnocli', $id, PDO::PARAM_INT);
-  $lignes->execute();
-  $unClient = $lignes->fetchObject('CLIENT');
-var_dump($unClient);
-return $unClient;
+  $ligne = $unObjetPdo->prepare($sql);
+  $ligne->bindValue('pnocli',$id,PDO::PARAM_INT);
+  $ligne->execute();
+  $unClient = $ligne->fetchObject('Client');
+  var_dump($unClient);
+  return $unClient;
+}
+function afficheTousClientsObjet($unObjetPdo){
+  $tableauClients = recupPlusieursObjetsClient($unObjetPdo);
+  echo "<p>liste des clients <br>";
+  foreach ($tableauClients as $unObjetClient){
+    echo $unObjetClient->afficheUnClient() . "<br>";
+    }
+}
+function recupPlusieursObjetsClient($unObjetPdo){
+  $sql = "select * from CLIENT";
+  $lignes = $unObjetPdo->query($sql);
+  if ($lignes->rowCount() > 0){
+    $tableauClients = $lignes->fetchAll(PDO::FETCH_CLASS, 'Client');
+    return $tableauClients;
+  } else {
+    throw new Exception ('Aucun client trouvé');
+  }
 }
 
 
--- TP1/index.php
+++ TP1/index.php
@@ -4,24 +4,36 @@
     <meta charset="utf-8">
     <title></title>
   </head>
-
   <?php
+  require 'autoload.php';
+  AutoLoader::Register();
+
+  require_once('vendor/autoload.php');
+  use Symfony\Component\Debug\Debug;
+  Debug::enable();
+
   try {
     define('RACINE', __DIR__);
     include_once('config/conf.php');
     include_once(INCLUDE_PATH . 'connect.inc.php');
+    include_once(INCLUDE_PATH . 'traitements.inc.php');
+
     $conn = connectionBd();
     echo "CONNEXION OP";
    ?>
 
   <body>
-<h1>CONTENU</h1>
-<p>
+  <div>
+    <p><?php recuperationUnClient($conn, 123); ?></p>
+    <p><?php afficheTousClients($conn);?> </p>
+    <p><?php $unClient=recupUnObjetClient($conn,123); ?></p>
+    <p><?php dump($unClient->afficheUnClient());?></p>
+    <p><?php afficheTousClientsObjet($conn); ?></p>
+  </div>
   <?php
-  include_once(INCLUDE_PATH . 'traitements.inc.php');
-  recuperationUnClient($conn, 123);
-  afficheTousClients($conn);
-  
+
+
+  dump($unClient->afficheUnClient());
   ?>
 </p>
   </body>
